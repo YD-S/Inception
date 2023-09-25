4f6ca99da0bf783d3df11740bc17265bef8dc45e
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2023-09-25 21:35:40 +0200

    aprende a escribir

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -2,14 +2,11 @@
 
 service mysql start 
 
-echo "CREATE DATABASE IF NOT EXISTS $msql_db_name ;" > msql_db.sql
-echo "CREATE USER IF NOT EXISTS '$msql_db1_user'@'%' IDENTIFIED BY '$msql_db_pwd' ;" >> msql_db.sql
-echo "GRANT ALL PRIVILEGES ON $msql_db_name.* TO '$msql_db_user'@'%' ;" >> msql_db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> msql_db.sql
+echo "CREATE DATABASE IF NOT EXISTS mariadb ;" > msql_db.sql
+echo "CREATE USER IF NOT EXISTS 'ysingh'@'%' IDENTIFIED BY '1234' ;" >> msql_db.sql
+echo "GRANT ALL PRIVILEGES ON mariadb.* TO 'ysingh'@'%' ;" >> msql_db.sql
 echo "FLUSH PRIVILEGES;" >> msql_db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> msql_db.sql
 
 mysql < msql_db.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
